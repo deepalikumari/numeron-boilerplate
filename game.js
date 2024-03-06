e81e20eabb086e277ce7509a06f830b11bc0d632
Deepali kumari <deepalikumari1234@gmail.com>
2024-03-06 09:57:26 +0530

    did chnages

--- game.js
+++ game.js
@@ -1,5 +1,82 @@
-// Iteration 2: Generate 2 random number and display it on the screen
+//Variables
+let num1 = document.getElementById("number1")
+let num2 = document.getElementById("number2")
+let number1;
+let number2;
 
-// Iteration 3: Make the options button functional
+
+
+
+
+// Iteration 2: Generate 2 random number and display it on the screen
+function randomNumbers() {
+    return Math.floor(Math.random() * 100) + 1
+}
+
+function displayRandomNumbers() {
+    number1 = randomNumbers()
+    number2 = randomNumbers()
+
+    num1.innerText = number1
+    num2.innerText = number2
+}
+
+displayRandomNumbers()
+
+
+
+
+
+
+// Iteration 3: Make the options button functional and Compare numbers
+var buttons = document.getElementById("buttons")
+let score = 0
+buttons.addEventListener("click", (e) => {
+
+    compareNumbers(e.target.id)
+})
+
+function compareNumbers(operation_id) {
+
+    if (operation_id == "greater-than" && number1 > number2 || operation_id == "equal-to" && number1 == number2 || operation_id == "lesser-than" && number1 < number2) {
+        time = 5
+        score++
+        displayRandomNumbers()
+    }
+    else {
+        gameover()
+    }
+}
+
+
+
+
+
+
+
 
 // Iteration 4: Build a timer for the game
+var timer = document.getElementById("timer")
+let time = 5
+
+function stopWatch() {
+    if (time<=0) {
+        gameover();
+    }
+    timer.innerText = time
+    time--
+}
+
+setInterval(stopWatch, 1000)
+
+
+
+
+
+
+
+//Gameover Function
+    function gameover() {
+        localStorage.setItem("score", score)
+        location.href = "gameover.html"
+    }
--- score.js
+++ score.js
@@ -1,1 +1,9 @@
 // Iteration 5: Store the player score and display it on the game over screen
+var scoreBox = document.getElementById("score-board")
+
+let play_again_button = document.getElementById("play-again-button")
+play_again_button.addEventListener("click" ,() => {
+     window.location.href="game.html"
+})
+
+scoreBox.innerText=localStorage.getItem("score")
